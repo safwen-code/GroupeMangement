8f71b4cb7acbd9ce63df1c59af28b75a82b3ce8a
safwen-code <safwendjebbi1234@gmail.com>
2021-11-03 02:20:17 +0100

    finish model components fist test

--- src/components/content/KMSforms/Addkm.jsx
+++ src/components/content/KMSforms/Addkm.jsx
@@ -1,29 +1,51 @@
 import React, { useState } from "react";
-
+import ModalKm from "./ModalKm";
 const Addkm = () => {
   const [kmForm, setkmForm] = useState({
     kmName: "",
     article: "",
-    users: {},
+    usersinfo: [],
     sableuse: [],
   });
+
+  const { kmName, article, usersinfo, sableuse } = kmForm;
+  const hundelchange = (e) => {
+    setkmForm({ ...kmForm, [e.target.name]: e.target.value });
+  };
   return (
     <div className="container border border-dark mt-3">
-      <div class="mb-3">
-        <label for="exampleFormControlInput1" class="form-label">
-          km Name
-        </label>
-        <input type="text" class="form-control" />
-      </div>
-      <div class="mb-3">
-        <label for="exampleFormControlInput1" class="form-label">
-          Article
-        </label>
-        <input type="text" class="form-control" />
-      </div>
-      <div className="border border-dark mt-3 d-flex justify-content-center mb-3">
-          hello users
-      </div>
+      <h6 className="text-center "> Add km Info</h6>
+      <form>
+        <div className="col d-flex justify-content-center">
+          <label className="form-label">Km Name</label>{" "}
+          <input
+            className="form-controle"
+            name="kmName"
+            value={kmName}
+            onChange={hundelchange}
+            type="text"
+          />{" "}
+          <label className="form-label">Article</label>{" "}
+          <input
+            className="form-controle"
+            type="text"
+            name="article"
+            value={article}
+            onChange={hundelchange}
+          />
+        </div>
+        <div className="border border-primary mt-3 mb-3 ms-2 me-2">
+          <h6 className="text-center text-primary mt-4 mb-3">
+            Add User to Km{" "}
+          </h6>
+          <div className="border border-danger mt-3 mb-3 d-flex justify-content-between">
+            <div>
+              <ModalKm usersinfo={usersinfo} />
+            </div>
+            <div>displayuser</div>
+          </div>
+        </div>
+      </form>
     </div>
   );
 };
---
+++ src/components/content/KMSforms/ModalKm.jsx
@@ -0,0 +1,99 @@
+import React, { useState } from "react";
+
+const ModalKm = ({ usersinfo }) => {
+  console.log("from modal", usersinfo);
+  const [UsersForm, setUsersForm] = useState({
+    name: "",
+    formée: "",
+    role: "",
+  });
+
+  const { name, formée, role } = UsersForm;
+  const hundelChange = (e) => {
+    setUsersForm({ ...UsersForm, [e.target.name]: e.target.value });
+  };
+  const hundelSubmit = (e) => {
+    usersinfo.push({...UsersForm});
+    console.log(usersinfo);
+  };
+  return (
+    <div>
+      <button
+        type="button"
+        class="btn btn-primary"
+        data-bs-toggle="modal"
+        data-bs-target="#exampleModal"
+      >
+        Launch demo modal
+      </button>
+
+      <div
+        class="modal fade"
+        id="exampleModal"
+        tabindex="-1"
+        aria-labelledby="exampleModalLabel"
+        aria-hidden="true"
+      >
+        <div class="modal-dialog">
+          <div class="modal-content">
+            <div class="modal-header">
+              <h5 class="modal-title" id="exampleModalLabel">
+                Modal title
+              </h5>
+              <button
+                type="button"
+                class="btn-close"
+                data-bs-dismiss="modal"
+                aria-label="Close"
+              ></button>
+            </div>
+            <div class="modal-body">
+              <label className="form-label">name</label>
+              <input
+                className="form-controle"
+                type="text"
+                name="name"
+                value={name}
+                onChange={hundelChange}
+              />
+              <label className="form-label">fromée</label>
+              <input
+                className="form-controle"
+                type="text"
+                name="formée"
+                value={formée}
+                onChange={hundelChange}
+              />
+              <label className="form-label">role</label>
+              <input
+                className="form-controle"
+                type="text"
+                name="role"
+                value={role}
+                onChange={hundelChange}
+              />
+            </div>
+            <div class="modal-footer">
+              <button
+                type="button"
+                class="btn btn-secondary"
+                data-bs-dismiss="modal"
+              >
+                Close
+              </button>
+              <button
+                type="button"
+                class="btn btn-primary"
+                onClick={hundelSubmit}
+              >
+                Save changes
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ModalKm;
