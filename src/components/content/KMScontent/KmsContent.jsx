1a808c00de377161c344eec61d0d7f7cd4b570d4
safwen-code <safwendjebbi1234@gmail.com>
2021-11-08 18:06:46 +0100

    edite kmItme to delet & edite

--- src/components/content/KMScontent/KmsContent.jsx
+++ src/components/content/KMScontent/KmsContent.jsx
@@ -8,7 +8,7 @@ const KmsContent = ({ Kmsinfo, loading }) => {
   return (
     <div className="row">
       {loading && Kmsinfo.length >= 0 ? (
-        Kmsinfo.map((km) => <KmsItem userinfo={km} id={km.id} />)
+        Kmsinfo.map((km) => <KmsItem kminfo={km} id={km.id} />)
       ) : (
         <h5>no kmsInof</h5>
       )}
--- src/components/content/KMScontent/KmsItem.jsx
+++ src/components/content/KMScontent/KmsItem.jsx
@@ -1,12 +1,21 @@
-import React from "react";
+import React, { useContext } from "react";
 import Moment from "react-moment";
 import SableuseInfo from "./SableuseInfo";
 import UserInfo from "./UserInfo";
-const KmsItem = ({
-  userinfo: { kmName, article, Date, users, sableuse },
-  id,
-}) => {
-  console.log("users from kmitem", users);
+import ContextKm from "../../context/ContextKm";
+import { useHistory } from "react-router-dom";
+
+const KmsItem = ({ kminfo, id }) => {
+  let history = useHistory();
+  const { kmName, article, Date, users, sableuse } = kminfo;
+  const contextKm = useContext(ContextKm);
+  console.log();
+  const { setCurrentKm, clearCurrentKm } = contextKm;
+
+  const HundelDelete = () => {
+    console.log("hundel delete");
+  };
+
   return (
     <>
       <div className="col  mt-3 ms-2 me-2 mb-3 " key={id}>
@@ -26,6 +35,22 @@ const KmsItem = ({
           sableuse.map((sabl) => (
             <SableuseInfo sable={sabl} idsabl={sabl.id} />
           ))}
+
+        <div className=" col ms-2 mt-3">
+          <button
+            type="button"
+            className="btn btn-secondary"
+            onClick={() => {
+              setCurrentKm(kminfo);
+              history.push("/addkm");
+            }}
+          >
+            Edite
+          </button>
+          <button className="ms-3 btn btn-danger" onClick={HundelDelete}>
+            Delete
+          </button>
+        </div>
       </div>
     </>
   );
